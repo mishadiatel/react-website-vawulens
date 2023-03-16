9c3b8e390132f784fffa24a1d734f9f037a89b83
mishadiatel <mishadiatel13@gmail.com>
2023-03-16 21:17:40 +0200

    added contact gallery and notfound pages

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,47 @@
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaTelegramPlane } from "react-icons/fa";
+
 import "./contact.css";
+import Header from "../../components/Header";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header title={"Get In Touch"} image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque non
+        numquam ipsa quae officiis velit.
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a
+              href="mailto:mishadiatel13@gmail.com"
+              target={"_blank"}
+              rel={"noreferrer nooopener"}
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mykhailo-diatel-56a578242/"
+              target={"_blank"}
+              rel={"noreferrer nooopener"}
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="https://t.me/mihyiii"
+              target={"_blank"}
+              rel={"noreferrer nooopener"}
+            >
+              <FaTelegramPlane />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,33 @@
+import Header from "../../components/Header";
 import "./gallery.css";
+import HeaderImage from "../../images/header_bg_3.jpg";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const galleryLength = 15;
+  const images = [];
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+
+  return (
+    <>
+      <Header title={"Our Gallery"} image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a et
+        voluptates possimus expedita. Deserunt!
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Gallery;
